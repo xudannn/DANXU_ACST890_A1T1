4d0dd138b703908a9c959cfca2cf6852c8b589c7
xudannn <48709617+xudannn@users.noreply.github.com>
2019-05-05 19:03:08 +1000

    Add files via upload

---
+++ DanXu44332971A1T1.py
@@ -0,0 +1,9 @@
+Python 3.7.3 (v3.7.3:ef4ec6ed12, Mar 25 2019, 16:52:21) 
+[Clang 6.0 (clang-600.0.57)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> a = 7
+>>> b = 15
+>>> import random
+>>> random.randint(a,b)
+8
+>>> 
