63e085eda2f315938958c18cdf7b4126932f2ff9
xudannn <48709617+xudannn@users.noreply.github.com>
2019-05-06 00:32:14 +1000

    Create DanXu44332971A1T1.py

---
+++ DanXu44332971A1T1.py
@@ -0,0 +1,44 @@
+#Q1
+scp -i 44332971.pem twain.txt ubuntu@ec2-54-252-141-71.ap-southeast-2.compute.amazonaws.com:
+sed 's/Huck/HucK/' twain.txt
+
+#Q2
+
+
+#Q3
+integer = int(input("Please enter an integer:"))
+
+if ((integer%400 == 0) or ((integer%4 == 0) and (integer%100 != 0))): 
+        print ("It’s a Leap year")
+else: 
+     print ("It’s not a Leap year")
+
+#Q4
+a = int(input("Please enter an integer a="))
+b = int(input("Please enter an integer b="))
+import random
+print (random.randint(a,b))
+
+#Q5
+m = int(input("Please enter an integer m="))
+d = int(input("Please enter an integer d="))
+if (m == 3 and (d <= 31 and d>=21)):
+    print ("True")
+elif (m == 4 and (d <= 30 and d>=1)):
+    print ("True")
+elif (m == 5 and (d <= 31 and d>=1)):
+    print ("True")
+elif (m == 6 and (d <= 19 and d>=1)):
+    print ("True")
+else:
+	print("False")
+
+#Q6
+import math
+t = float(input("Please enter the number of years t = "))
+P = float(input("Please enter the principla P = "))
+r = float(input("Please enter the annual interest rate r = "))
+e = math.e
+FV = P * (e ** (r * t))
+print FV
+
